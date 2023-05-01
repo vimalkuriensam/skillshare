4a54df99e9bbc24869216494035d008f7ab4330f
vimalkuriensam <vimalkuriensam@gmail.com>
2023-05-01 14:05:49 +0100

    configured redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-const App = () => {
-  return (
-    <div>
-      <div>Main file App.js</div>
-    </div>
-  );
-};
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store/configureStore";
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <div>Here</div>
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
