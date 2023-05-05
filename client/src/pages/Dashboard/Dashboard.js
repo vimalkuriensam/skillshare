2db80a80005ff6da765623a9c2073046b20752fa
vimalkuriensam <vimalkuriensam@gmail.com>
2023-05-05 04:46:58 +0100

    added alegreya sans sc regular font

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Title } from "../../components";
+import { Text, Title } from "../../components";
 
 const Dashboard = () => {
   return (
@@ -7,8 +7,24 @@ const Dashboard = () => {
       <div>
         <Title variant="sbi-40-1">Welcome Back</Title>
       </div>
-      <div>
-        <Title>SKILLSEARCH</Title>
+      <div style={{ background: "#000" }}>
+        <Title variant="osb-49-1">SKILLSEARCH</Title>
+        <div>
+          <Text variant="ir-20-1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            pulvinar scelerisque lorem, fermentum lacinia velit pulvinar ut.
+            Vivamus placerat maximus auctor. Sed cursus tristique urna, a
+            lobortis diam interdum id. Morbi tincidunt urna elementum, mollis
+            libero vel, pharetra mauris. Duis lacinia felis ac massa efficitur,
+            sed consequat libero tincidunt. Duis a arcu fermentum, posuere nunc
+            sit amet, pretium neque. Etiam in lobortis dolor, eu commodo velit.
+            Integer bibendum malesuada dignissim. Quisque sagittis massa quis
+            dignissim placerat. Donec molestie ex at elit imperdiet, nec maximus
+            eros faucibus. Fusce pretium metus est, sit amet facilisis augue
+            bibendum suscipit. Cras lobortis luctus ex, ac aliquet dolor semper
+            eu. Vestibulum rutrum lobortis mi, id finibus sapien accumsan vel.
+          </Text>
+        </div>
       </div>
     </div>
   );
