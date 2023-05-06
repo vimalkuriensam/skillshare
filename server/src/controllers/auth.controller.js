c5fefe9ab180ce2eeeb6c699b57cd49306b25f79
vimalkuriensam <vimalkuriensam@gmail.com>
2023-05-06 12:20:44 +0100

    added pasword hashing

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const { InsertUser } = require("../db/query");
 
@@ -12,8 +13,13 @@ const LoginController = async (req, res) => {
 const RegisterController = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
     if (!validator.isEmail(email)) throw { message: "INVALID_EMAIL_FORMAT" };
-    const result = await InsertUser({ username, email, password });
+    const result = await InsertUser({
+      username,
+      email,
+      password: hashedPassword,
+    });
     if (result) res.status(201).send({ message: "USER CREATED" });
   } catch (e) {
     res.status(500).send({ error: e.message });
