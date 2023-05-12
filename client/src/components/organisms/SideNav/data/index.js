c59a91573d87e56466476db66e95373194555bf7
vimalkuriensam <vimalkuriensam@gmail.com>
2023-05-12 13:00:51 +0100

    added dashboard icon

---
+++ client/src/components/organisms/SideNav/data/index.js
@@ -0,0 +1,17 @@
+export const NAVCONTENTS = [
+  {
+    title: "dashboard",
+    link: "/dashboard",
+    icon: "Dashboard",
+  },
+  {
+    title: "profile",
+    link: "/profile",
+    icon: "Notepad",
+  },
+  {
+    title: "settings",
+    link: "/settings",
+    icon: "Gear",
+  },
+];
