8046fecbf98f46d9bd0bd38e0d900f1a26e191eb
vimalkuriensam <vimalkuriensam@gmail.com>
2023-05-12 13:30:03 +0100

    added almarai font

--- client/src/components/organisms/SideNav/SideNav.js
+++ client/src/components/organisms/SideNav/SideNav.js
@@ -4,9 +4,12 @@ import { toggleNavbar } from "../../../redux/actions/utils.action";
 import { Capitalize } from "../../../utils/data";
 import { Icon, Title } from "../../atoms";
 import SidenavTitle from "./container/SidenavTitle";
+import { NAVCONTENTS } from "./data";
 
 const SideNav = ({ dispatch, navbar }) => {
   const onHandleNavbar = () => dispatch(toggleNavbar());
+
+  const onHandleIcon = (link) => console.log(link);
 
   return (
     <div
@@ -15,6 +18,14 @@ const SideNav = ({ dispatch, navbar }) => {
       }`}
     >
       <SidenavTitle navbar={navbar} onHandleNavbar={onHandleNavbar} />
+      <div>
+        {NAVCONTENTS.map(({ title, link, icon }, index) => (
+          <div className="header__nav" key={index} onClick={onHandleIcon.bind(this, link)}>
+            <Icon name={icon} />
+            <Title className="u-margin-left-10" variant="alr-20-1">{Capitalize(title)}</Title>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
