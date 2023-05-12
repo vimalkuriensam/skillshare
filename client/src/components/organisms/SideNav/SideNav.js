f8f110ea5a173a22ab04385533dbc564fc878f6c
vimalkuriensam <vimalkuriensam@gmail.com>
2023-05-12 11:51:23 +0100

    added hamburger icon

--- client/src/components/organisms/SideNav/SideNav.js
+++ client/src/components/organisms/SideNav/SideNav.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Capitalize } from "../../../utils/data";
-import { Title } from "../../atoms";
+import { Icon, Title } from "../../atoms";
 
 const SideNav = () => {
-  return <div className="header__sidenav header__sidenav--wide">
-    <div>
-      <Title variant="osb-30-1">{Capitalize("SkillSearch")}</Title>
+  return (
+    <div className="header__sidenav header__sidenav--wide">
+      <div className="header__sidenavContainer">
+        <Title variant="osb-30-1">{Capitalize("SkillSearch")}</Title>
+        <Icon name="Hamburger" />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default SideNav;
---
+++ client/src/utils/icons/Hamburger.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hamburger = () => (
+  <svg
+    width="30"
+    height="18"
+    viewBox="0 0 30 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="30" height="4" rx="2" fill="white" />
+    <rect y="7" width="30" height="4" rx="2" fill="white" />
+    <rect y="14" width="30" height="4" rx="2" fill="white" />
+  </svg>
+);
+
+export default Hamburger;
