37b40b7e1a2ac07885b816aaf119935103edeb2a
vimalkuriensam <vimalkuriensam@gmail.com>
2023-05-13 20:00:05 +0100

    added countries json file

--- server/src/db/data/index.js
+++ server/src/db/data/index.js
@@ -1,6 +1,6 @@
 const DATA = {
   CREATE_USER_TABLE: `CREATE TABLE IF NOT EXISTS users(
-        user_id SERIAL PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(50) UNIQUE NOT NULL,
         first_name VARCHAR(20),
@@ -10,6 +10,27 @@ const DATA = {
         phone VARCHAR(11) UNIQUE,
         password VARCHAR(100) NOT NULL
     );`,
+
+  CREATE_ADDRESS_TABLE: `CREATE TABLE IF NOT EXISTS address(
+        id SERIAL PRIMARY KEY,
+        address_line1 VARCHAR(50),
+        address_line2 VARCHAR(50),
+        city_id INT,
+        pincode INT,
+        user_id INT,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    );`,
+
+  CREATE_TABLE_COUNTRIES: `CREATE TABLE IF NOT EXISTS countries(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) UNIQUE NOT NULL
+  )`,
+  CREATE_TABLE_CITIES: `CREATE TABLE IF NOT EXISTS cities(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    country_id INT NOT NULL,
+    FOREIGN KEY(country_id) REFERENCES countries(id)
+  )`,
   INSERT_USER: `INSERT INTO users(username, email, password)
                 VALUES ($1, $2, $3);`,
   GET_USER_USERNAME: `SELECT * FROM users
--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,4 +1,5 @@
 const { DATA } = require("./data");
+const fs = require("fs/promises");
 
 const Pool = require("pg").Pool;
 
@@ -13,11 +14,75 @@ const pool = new Pool({
 const createTables = async () => {
   try {
     console.log("CREATING TABLES...");
-    const res = await pool.query(DATA.CREATE_USER_TABLE);
-    if (res?.command) console.log("USER TABLE CREATED OR ALREADY EXIST");
+    await createCountries();
+    await createCities();
+    await createUsers();
+    await createAddress();
+    await insertCountryAndCity();
   } catch (e) {
     console.log(e.message);
     await pool.end();
+  }
+};
+
+const insertCountryAndCity = async () => {
+  try {
+    const resp = await fs.readFile("src/db/data/countries.json");
+    console.log("resp", JSON.parse(resp));
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const createAddress = async () => {
+  try {
+    const res = await pool.query(DATA.CREATE_ADDRESS_TABLE);
+    if (res?.command) {
+      console.log("ADDRESS TABLE CREATED OR ALREADY EXIST...");
+      return true;
+    }
+  } catch (e) {
+    console.log(e.message);
+    throw { message: e.message };
+  }
+};
+
+const createUsers = async () => {
+  try {
+    const res = await pool.query(DATA.CREATE_USER_TABLE);
+    if (res?.command) {
+      console.log("USER TABLE CREATED OR ALREADY EXIST...");
+      return true;
+    }
+  } catch (e) {
+    console.log(e.message);
+    throw { message: e.message };
+  }
+};
+
+const createCountries = async () => {
+  try {
+    const res = await pool.query(DATA.CREATE_TABLE_COUNTRIES);
+    if (res?.command) {
+      console.log("COUNTRY TABLE CREATED OR ALREADY EXIST...");
+      return true;
+    }
+  } catch (e) {
+    console.log(e.message);
+    return false;
+  }
+};
+
+const createCities = async () => {
+  try {
+    const res = await pool.query(DATA.CREATE_TABLE_CITIES);
+    if (res?.command) {
+      console.log("CITY TABLE CREATED OR ALREADY EXIST...");
+      return true;
+    }
+  } catch (e) {
+    console.log(e.message);
+    return false;
   }
 };
 
